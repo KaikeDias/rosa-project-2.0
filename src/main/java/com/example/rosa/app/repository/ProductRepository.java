40b3209b5b2b93eae5525c270fe64aa9f3a6dc3d
Kaike <kaikedias98@gmail.com>
2023-09-16 18:02:37 -0300

    feat: add ProductRepository

---
+++ src/main/java/com/example/rosa/app/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.example.rosa.app.repository;
+
+import com.example.rosa.app.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {}
