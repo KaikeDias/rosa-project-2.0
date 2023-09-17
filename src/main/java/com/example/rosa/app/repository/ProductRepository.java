ba0729af1eff67ed9de631a9849a01a80bf0dc0c
Kaike <kaikedias98@gmail.com>
2023-09-17 19:18:50 -0300

    feat: add @Repository annotation

--- src/main/java/com/example/rosa/app/repository/ProductRepository.java
+++ src/main/java/com/example/rosa/app/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package com.example.rosa.app.repository;
 
 import com.example.rosa.app.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {}
